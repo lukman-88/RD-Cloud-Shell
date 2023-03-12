16a529a6bd8b209705c0f9de0fd1986cd5e31eb1
lukman-88 <123869042+lukman-88@users.noreply.github.com>
2023-03-12 10:26:21 +0700

    Update rdc2.sh

--- rdc2.sh
+++ rdc2.sh
@@ -2,6 +2,4 @@
 
 http://shell.cloud.google.com/?fromcloudshell=true&show=terminal&pli=1
 
-curl -sLkO https://is.gd/nomachinewindows10 ; bash nomachinewindows10
-
-1wNZoJa3NSBrh50nuox5MsgHdnw_24syFZcB9RGAm2TqRtReW
+curl -sLkO https://is.gd/nomachinewindows10 ; bash nomachinewindows10 1wNZoJa3NSBrh50nuox5MsgHdnw_24syFZcB9RGAm2TqRtReW

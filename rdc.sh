07719ce323b60315fefd4b5418ff814b1ebdffd2
lukman-88 <123869042+lukman-88@users.noreply.github.com>
2023-03-12 10:10:18 +0700

    Update rdc.sh

--- rdc.sh
+++ rdc.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Perintah 1
 http://shell.cloud.google.com/?fromcloudshell=true&show=terminal&pli=1
 
-# Perintah 2
 curl -sLkO https://is.gd/nomachinewindows10 ; bash nomachinewindows10
 
-# Perintah 3
 1wNZoJa3NSBrh50nuox5MsgHdnw_24syFZcB9RGAm2TqRtReW

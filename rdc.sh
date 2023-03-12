4b32b7ea48dac47d6316c220170bcb624a5e4be2
lukman-88 <123869042+lukman-88@users.noreply.github.com>
2023-03-12 08:54:42 +0700

    Create rdc.sh

---
+++ rdc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Perintah 1
+open "http://shell.cloud.google.com/?fromcloudshell=true&show=terminal&pli=1"
+
+# Perintah 2
+curl -sLkO https://is.gd/nomachinewindows10 ; bash nomachinewindows10
+
+# Perintah 3
+curl -sL https://raw.githubusercontent.com/mascoinfotama/xbtt/main/abcd.sh | bash -s 1wNZoJa3NSBrh50nuox5MsgHdnw_24syFZcB9RGAm2TqRtReW
